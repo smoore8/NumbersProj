256a3f646ef4834b764c144db402768453ce62d9
smoore8 <savionmoore8@gmail.com>
2014-05-06 10:11:19 -0400

    5/6/14

---
+++ Numbers.cs/Numbers.cs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Numbers.cs
+{
+    class Program
+    {
+        static void Sum (int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void Difference(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        static void main ()
+        {
+            int q=30 t=39;
+            sum(q, t);
+
+    }
+}
